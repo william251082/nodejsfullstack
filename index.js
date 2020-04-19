ceaedd5eb2b9390af72e8b737013cdadc155a145
william <william.del.rosario@iceshop.nl>
2020-04-19 11:14:41 +0200

    Parsing request paths

--- index.js
+++ index.js
@@ -5,10 +5,23 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // Configure the server to respond to all requests with a string
 var server = http.createServer(function(req,res){
+
+    // Parse the url
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end('Hello World!\n');
+
+    // Log the request/response
+    console.log('Request received on path: '+trimmedPath);
 });
 
 // Start the server
